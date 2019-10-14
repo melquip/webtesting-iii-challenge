7fe070790b2f0d6624124e02a473add784529670
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-14 14:30:55 +0100

    Added tests to Controls component

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -7,12 +7,36 @@ import Controls from './Controls';
 let wrapper;
 
 afterEach(rtl.cleanup);
+
 beforeEach(() => {
-	wrapper = rtl.render(<Controls />);
+	wrapper = rtl.render(<Controls
+		locked={false}
+		closed={false}
+	/>);
 });
 
 describe('Controls', () => {
-	it('works', () => {
-
+	it('defaults gate to be open and unlocked', () => {
+		expect(wrapper.queryByText(/Lock Gate/i)).toBeInTheDocument();
+		expect(wrapper.queryByText(/Close Gate/i)).toBeInTheDocument();
+	});
+	it('provides buttons to toggle the closed and locked states', () => {
+		let toggleLockedBtn = wrapper.getByTestId('toggleLocked');
+		let toggleClosedBtn = wrapper.getByTestId('toggleClosed');
+		expect(toggleLockedBtn).toBeInTheDocument();
+		expect(toggleClosedBtn).toBeInTheDocument();
+	});
+	it('the closed toggle button is disabled if the gate is locked', () => {
+		rtl.cleanup();
+		wrapper = rtl.render(<Controls 
+			locked={true}
+			closed={true}
+		/>);
+		let toggleClosedBtn = wrapper.getByTestId('toggleClosed');
+		expect(toggleClosedBtn).toBeDisabled();
+	});
+	it('the locked toggle button is disabled if the gate is open', () => {
+		let toggleLockedBtn = wrapper.getByTestId('toggleLocked');
+		expect(toggleLockedBtn).toBeDisabled();
 	});
 });
