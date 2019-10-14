7c8a516d32f179613257c6a9f2b70c6c870ccbb6
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-14 13:18:37 +0100

    Added initial test code to other components

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,18 @@
-// Test away!
+// Test away
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Controls from './Controls';
+
+let wrapper;
+
+afterEach(rtl.cleanup);
+beforeEach(() => {
+	wrapper = rtl.render(<Controls />);
+});
+
+describe('Controls', () => {
+	it('works', () => {
+
+	});
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
 // Test away
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+
+let wrapper;
+
+afterEach(rtl.cleanup);
+beforeEach(() => {
+	wrapper = rtl.render(<Dashboard />);
+});
+
+describe('Dashboard', () => {
+	it('works', () => {
+
+	});
+});
